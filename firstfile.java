9cd1610599f01eff5fcdd9d6a2846a3777babc0c
beherasurya <beherasuryakanta18@gmail.com>
2023-02-24 11:19:37 +0530

    first file updated

---
+++ firstfile.java
@@ -0,0 +1,9 @@
+class First
+{
+    public static void main(String[] args) {
+        System.out.println("Hii Guys");
+
+        int firstNumber =90;
+        System.out.println(firstNumber+90+Integer.parseInt("10"));
+    }
+}
